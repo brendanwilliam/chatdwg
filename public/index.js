cef29c4e41e5e4d641e882a27e6496e5c32aff18
brendanwilliam <brendanwillkeane@gmail.com>
2023-05-06 13:43:42 -0700

    replaced SVG with wordmark

--- public/index.js
+++ public/index.js
@@ -4,3 +4,39 @@
  * Purpose: Add menu button interactivity to navbar
  */
 
+'use strict';
+(function() {
+
+  window.addEventListener('load', init);
+
+  function init() {
+    qs('')
+  }
+
+
+  /* ------------------------------ Helper Functions  ------------------------------ */
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} res - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   *                    Promise result
+   */
+   async function statusCheck(res) {
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return res;
+  }
+
+  /**
+   * Returns the element that has the matches the selector passed.
+   * @param {string} selector - selector for element
+   * @return {object} DOM object associated with selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+})();
